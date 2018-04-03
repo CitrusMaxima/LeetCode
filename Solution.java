33e522a0858a9491aef4a6d50aca3121a3c13ccf
CitrusMaxima <2663753900@qq.com>
2018-04-03 13:42:47 +0800

    LeetCode两数之和及两数相加

--- Solution.java
+++ Solution.java
@@ -6,6 +6,47 @@ import java.util.List;
 
 class Solution {
 
+    //两数之和
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for (int i=0; i<nums.length-1; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    //两数相加
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        if(carry>0){
+            curr.next=new ListNode(carry);
+        }
+
+        return dummyHead.next;
+    }
+
+    //三数之和
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
@@ -62,6 +103,7 @@ class Solution {
         return result;
     }
 
+    //最接近的三数之和
     public int threeSumClosest(int[] nums, int target) {
 
         Arrays.sort(nums);
