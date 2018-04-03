9df9e8be66cd0fe1114080c37703450c9dcb4434
CitrusMaxima <2663753900@qq.com>
2018-04-03 13:48:50 +0800

    LeetCode盛水最多的容器

--- Solution.java
+++ Solution.java
@@ -227,5 +227,22 @@ class Solution {
 
         return true;
     }
+
+    //盛水最多的容器
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length-1;
+        int max = 0;
+        while(left<right){
+            max = Math.max(max, Math.min(height[left], height[right])*(right-left));
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max;
+    }
 }
 
