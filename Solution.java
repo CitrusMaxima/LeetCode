ae34de250368d434b9bd968016d2d4ce2504cc60
CitrusMaxima <2663753900@qq.com>
2018-04-03 13:50:28 +0800

    LeetCode最长公共前缀

--- Solution.java
+++ Solution.java
@@ -281,5 +281,25 @@ class Solution {
 
         return result;
     }
+
+    //最长公共前缀
+    public String longestCommonPrefix(String[] strs) {
+
+        if(strs.length==0||strs[0].length()==0)
+            return "";
+        if(strs.length==1)
+            return strs[0];
+        int i=0;
+        while(i<strs[0].length())
+        {
+            for(int j=1;j<strs.length;j++)
+            {
+                if(strs[j].length()<i+1||strs[j].charAt(i)!=strs[0].charAt(i))
+                    return strs[0].substring(0,i);
+            }
+            i++;
+        }
+        return strs[0];
+    }
 }
 
