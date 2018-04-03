fd5a5e6fec5fda8b32069726a9b3adb9e8405b7b
CitrusMaxima <2663753900@qq.com>
2018-04-03 13:46:57 +0800

    LeetCode最长回文子串

--- Solution.java
+++ Solution.java
@@ -162,5 +162,34 @@ class Solution {
         return max;
     }
 
+    //最长回文子串
+    private int max = 0;
+    private String res = "";
+
+    public String longestPalindrome(String s) {
+        if (s.length() == 1) { return s; }
+        for (int i = 0; i < s.length()-1; i++) {
+            checkPalindromeExpand(s,i,i);
+            checkPalindromeExpand(s,i,i+1);
+        }
+        return res;
+    }
+
+    public void checkPalindromeExpand(String s, int low, int high) {
+        while (low >= 0 && high < s.length()) {
+            if (s.charAt(low) == s.charAt(high)) {
+                if (high - low + 1 > max) {
+                    max = high - low + 1;
+                    res = s.substring(low,high+1);
+                }
+                low--; high++;
+            } else {
+                return;
+            }
+        }
+    }
+
+
+
 }
 
