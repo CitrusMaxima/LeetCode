65295702e001ee8da03f9f3ea85d98178de5815d
CitrusMaxima <2663753900@qq.com>
2018-04-03 13:52:43 +0800

    LeetCode最大子序和

--- Solution.java
+++ Solution.java
@@ -356,5 +356,24 @@ class Solution {
                 return i;
         return i;
     }
+
+    //最大子序和
+    public int maxSubArray(int[] nums) {
+
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max)
+                max = sum;
+            else if (sum < 0)
+                sum = 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0)
+                return max;
+        }
+        return Arrays.stream(nums).max().getAsInt();
+    }
 }
 
