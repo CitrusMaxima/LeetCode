1be74df2563684176b4bd253aba5b1ed4b224d57
CitrusMaxima <2663753900@qq.com>
2018-04-06 14:00:10 +0800

    LeetCode旋转图像

--- Solution.java
+++ Solution.java
@@ -463,5 +463,21 @@ class Solution {
         }
         return list;
     }
+
+    //旋转图像
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = i; j < n - 1 - i; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = tmp;
+            }
+        }
+    }
+
+    
 }
 
