0c92dc164a54fc42aae0dad35e5062f94aeeea08
CitrusMaxima <2663753900@qq.com>
2018-04-10 14:51:24 +0800

    LeetCode 括号生成

--- Main.java
+++ Main.java
@@ -1,22 +1,13 @@
 package leetcode;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-
-        head = solution.swapPairs(head);
-
-        System.out.println(head.val);
-        System.out.println(head.next.val);
-        System.out.println(head.next.next.val);
-        System.out.println(head.next.next.next.val);
-        System.out.println(head.next.next.next.next.val);
+        List<String> result = solution.generateParenthesis(3);
+        System.out.println(result);
     }
 }
--- Solution.java
+++ Solution.java
@@ -587,5 +587,26 @@ class Solution {
 
         return head;
     }
+
+    //括号生成
+    public List<String> generateParenthesis(int n) {
+        Set<String> res = new HashSet<String>();
+        if (n == 0) {
+            res.add("");
+        } else {
+            List<String> pre = generateParenthesis(n - 1);
+            for (String str : pre) {
+                for (int i = 0; i < str.length(); ++i) {
+                    if (str.charAt(i) == '(') {
+                        str = str.substring(0, i + 1) + "()" + str.substring(i + 1, str.length());
+                        res.add(str);
+                        str = str.substring(0, i + 1) +  str.substring(i + 3, str.length());
+                    }
+                }
+                res.add("()" + str);
+            }
+        }
+        return new ArrayList(res);
+    }
 }
 
