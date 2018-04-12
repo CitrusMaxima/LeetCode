f0b7561787303a8ea97a553a091f16aed721ebc1
CitrusMaxima <2663753900@qq.com>
2018-04-12 16:27:25 +0800

    LeetCode 字符串相乘

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 package leetcode;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        List<String> result = solution.generateParenthesis(3);
+        String result = solution.multiply("11","12");
         System.out.println(result);
     }
 }
