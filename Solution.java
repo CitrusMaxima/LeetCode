9772fdae3cd26a171e8fc27c11e159bd289cb175
CitrusMaxima <2663753900@qq.com>
2018-04-16 14:44:46 +0800

    LeetCode 旋转链表

--- Solution.java
+++ Solution.java
@@ -796,5 +796,31 @@ class Solution {
 
         return list;
     }
+
+    //旋转链表
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null)
+            return null;
+
+        int n = 1;
+        ListNode cur = head;
+        while (cur.next != null) {
+            ++n;
+            cur = cur.next;
+        }
+
+        cur.next = head;
+        int m = n - k % n;
+
+        for (int i=0; i<m; ++i) {
+            cur = cur.next;
+        }
+        ListNode newHead = cur.next;
+        cur.next = null;
+
+        return newHead;
+    }
+
 }
 
