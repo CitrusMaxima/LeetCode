c0bb3fba82864638c1ec21c3e03d9f03a56495d9
CitrusMaxima <2663753900@qq.com>
2018-04-18 14:32:37 +0800

    LeetCode 最小路径和

--- Solution.java
+++ Solution.java
@@ -883,4 +883,26 @@ class Solution {
         return obstacleGrid[m-1][n-1];
     }
 
+    //最小路径和
+    public int minPathSum(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] result = new int[m][n];
+        result[0][0] = grid[0][0];
+
+        for (int i = 1; i < m; i++)
+            result[i][0] = grid[i][0] + result[i-1][0];
+        for (int j = 1; j < n; j++)
+            result[0][j] = grid[0][j] + result[0][j-1];
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                result[i][j] = grid[i][j] + Math.min(result[i-1][j],result[i][j-1]);
+            }
+        }
+
+        return result[m-1][n-1];
+    }
+
 }
