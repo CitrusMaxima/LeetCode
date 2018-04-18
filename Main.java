32c7f23e0a1ff739d506b2322312174033987ec9
CitrusMaxima <2663753900@qq.com>
2018-04-18 14:21:04 +0800

    LeetCode 不同路径 II

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        int result = solution.uniquePaths(4,3);
+        int[][] metrix = {{0,0,1,1},{0,0,1,0},{0,0,0,0}};
+
+        int result = solution.uniquePathsWithObstacles(metrix);
         System.out.println(result);
     }
 }
