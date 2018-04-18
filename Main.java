c2dc8522db5ac4caf63d6219a97a767c98064132
CitrusMaxima <2663753900@qq.com>
2018-04-18 13:55:14 +0800

    LeetCode 不同路径

--- Main.java
+++ Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        int[] x = new int[]{3,2,1,0,4};
-        boolean result = solution.canJump(x);
+        int result = solution.uniquePaths(4,3);
         System.out.println(result);
     }
 }
