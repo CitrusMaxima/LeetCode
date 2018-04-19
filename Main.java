b65d533741e4ae48b6163ac3f8ce1ac2b0710725
CitrusMaxima <2663753900@qq.com>
2018-04-19 14:05:37 +0800

    LeetCode 子集

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
 package leetcode;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        int[][] metrix = {{1}};
+        int[] x = new int[]{1,2,3};
 
-        boolean result = solution.searchMatrix(metrix,0);
+        List<List<Integer>> result = solution.subsets(x);
         System.out.println(result);
     }
 }
