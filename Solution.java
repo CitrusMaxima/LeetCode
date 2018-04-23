54a06b01c67f08de58be86e942fdb04cd608b3f2
CitrusMaxima <2663753900@qq.com>
2018-04-23 10:15:06 +0800

    LeetCode 二叉树的层次遍历

--- Solution.java
+++ Solution.java
@@ -1069,4 +1069,38 @@ class Solution {
         return leftResult && rightResult;
     }
 
+    //二叉树的层次遍历
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+        if(root == null)
+            return list;
+
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();  //定义一个LinkedList集合，其类型是TreeNode
+        queue.offer(root);  //首先把根进入队列中
+        while(!queue.isEmpty())  //队列不为空的
+        {
+            int num = queue.size();  //队列的长度；
+            List<Integer> templist = new ArrayList<Integer>();   //定义一个ArrayList集合
+
+            while(num > 0)//首先判断num是否大于0先获取每一层的结点个数。
+            {
+                TreeNode node = queue.peek(); //查看队头元素，但是不删除队头的元素，队列是先进先出的规则
+                if(node.left != null) //该节点的左孩子不为空
+                    queue.offer(node.left); //将左孩子进入队尾
+
+                if(node.right != null)   //如果该节点的右孩子不为空
+                    queue.offer(node.right);  //将右孩子进入队尾。
+
+                int data = queue.poll().val;  //将节点从队头出队// 。
+                templist.add(data); //将出的节点加入到tempList集中
+                num--;  //然后每次减1.
+            }
+            list.add(templist);
+        }
+
+        return list;
+    }
+
+
 }
